6df3f72e02c061c3ef5754885c6643bac6691101
tom buckley <buckley.tom@gmail.com>
2014-06-26 21:50:31 -0700

    more testing

--- simplegeocoder_server.py
+++ simplegeocoder_server.py
@@ -7,11 +7,12 @@ app.debug = True
 
 @app.route('/', methods=['GET','POST'])
 def hello():
+	if request.method=='GET':
+		return "hello"
 	if request.method=='POST':
 		# sentence = request.form['fileupload']
 		# #.translate(string.maketrans("",""), string.punctuation).split()
 		# return jsonify(text=geocode_text(sentence))
-	if request.method=='GET':
 		return "hello"
 if __name__ == "__main__":
     app.run()
