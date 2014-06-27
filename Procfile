689a75a13564f40c60c046e62221e43c79eb9648
tom buckley <buckley.tom@gmail.com>
2014-06-26 18:55:12 -0700

    added server stuff

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn simplegeocoder_server:app
---
+++ simplegeocoder_server.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, url_for, jsonify
+from geocoder import *
+import string
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/', methods=['GET','POST'])
+def hello():
+	if request.method=='POST':
+		sentence = request.form['fileupload']
+		#.translate(string.maketrans("",""), string.punctuation).split()
+		return jsonify(text=geocode_text(sentence))
+	if request.method=='GET':
+		return "hello"
+if __name__ == "__main__":
+    app.run()
