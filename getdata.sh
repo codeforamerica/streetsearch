ec1603bf6c7881f9080d254189176b3aadda1f6b
tom buckley <buckley.tom@gmail.com>
2014-08-20 23:25:29 -0700

    fixed getdata.sh so that it pulls data from census and sets up postgis table for roads data for mesa

--- getdata.sh
+++ getdata.sh
@@ -1,16 +1,19 @@
 #get maricopa county roads from us census:
-wget ftp://ftp2.census.gov/geo/tiger/TIGER2013/ROADS/tl_2013_04013_roads.zip
-unzip tl_2013_04013_roads.zip
+# wget ftp://ftp2.census.gov/geo/tiger/TIGER2013/EDGES/tl_2013_04013_edges.zip
+# unzip tl_2013_04013_edges.zip
 
 export SHAPE_ENCODING="ISO-8859-1"
-ogr2ogr tl_2013_04013_roads2.shp tl_2013_04013_roads.shp -lco ENCODING=UTF-8
+ogr2ogr tl_2013_04013_edges2.shp tl_2013_04013_edges.shp -lco ENCODING=UTF-8
 
 createdb tiger
 psql -d tiger -c "CREATE EXTENSION postgis;"
 
 #import to postgis
 #shp2pgsql tl_2013_04013_roads.shp roads > roads.sql
-shp2pgsql -W "LATIN1" tl_2013_04013_roads2.shp roads > roads.sql
+shp2pgsql -W "LATIN1" -s 4269 tl_2013_04013_edges2.shp roads > roads.sql
+
+#clean up
+#rm tl_2013_04013_edges*
 
 psql -d tiger -f roads.sql
 
@@ -18,13 +21,22 @@ psql -d tiger -f roads.sql
 wget ftp://ftp2.census.gov/geo/tiger/TIGER2013/FEATNAMES/tl_2013_04013_featnames.zip
 unzip tl_2013_04013_featnames.zip
 
-#FIGURE OUT HOW TO get feature names dbf in postgres WITH ogr2ogr
-#import to postgis
-#shp2pgsql tl_2013_04013_roads.shp roads > roads.sql
+# FIGURE OUT HOW TO get feature names dbf in postgres WITH ogr2ogr
+# import to postgis
+# shp2pgsql tl_2013_04013_roads.shp roads > roads.sql
+# shp2pgsql -W "LATIN1" tl_2013_04013_featnames.shp roadnames > roadnames.sql
+#ogr2ogr -f "PostgreSQL" PG:"host=localhost dbname=tiger" tl_2013_04013_featnames.dbf -nln "featnames" -lco ENCODING=UTF-8
+
 shp2pgsql -W "LATIN1" tl_2013_04013_featnames.shp roadnames > roadnames.sql
 
 psql -d tiger -f roadnames.sql
 
+#clean up
+rm tl_2013_04013_featnames*
+
 #ogr2ogr -f "PostgreSQL" PG:"host=localhost dbname=tigert" sometable.dbf -nln "sometable"
 
+ogr2ogr -f "PostgreSQL" PG:"host=localhost dbname=tiger" "http://services2.arcgis.com/1gVyYKfYgW5Nxb1V/ArcGIS/rest/services/MesaAzCouncilDistricts/FeatureServer/0/query?where=objectid+%3D+objectid&outfields=*&f=json" OGRGeoJSON -nln "mesa_boundaries" -t_srs "EPSG:4269"
+
 psql -d tiger -a -f postgis_union_length.sql
+
--- postgis_union_length.sql
+++ postgis_union_length.sql
@@ -1,8 +1,11 @@
+CREATE TABLE namedroads AS SELECT roadnames.predirabrv as prefix, roadnames.suftypabrv as suffix, roadnames.name as name, roads.fullname as fullname, roads.geom FROM roadnames, roads WHERE roadnames.tlid = roads.tlid;
+
 --
-CREATE TABLE roadnames AS SELECT featnames.predirabrv as prefix, featnames.suftypabrv as suffix, featnames.name as name, roads.fullname as fullname, roads.geom FROM featnames, roads WHERE featnames.tlid = roads.tlid;
+CREATE TABLE mesaroads AS SELECT fullname, namedroads.geom FROM mesa_boundaries, namedroads WHERE ST_DWithin(mesa_boundaries.wkb_geometry, namedroads.geom, 0);
 
 -- group road rows by name
-CREATE TABLE roadlengths as select st_union(geom) as geom, fullname from roads group by fullname;
+CREATE TABLE roadlengths as select st_union(geom) as geom, fullname from mesaroads group by fullname;
+SELECT UpdateGeometrySRID('roadlengths','geom',4296);
 --
 -- add column for length
 ALTER TABLE roadlengths ADD COLUMN length NUMERIC(12,2);
@@ -10,4 +13,10 @@ ALTER TABLE roadlengths ADD COLUMN length NUMERIC(12,2);
 -- populate length of roads
 UPDATE roadlengths SET length = ST_Length(ST_Transform(geom,32612));
 
-SELECT fullname, ST_ASGeoJSON(geom) FROM roadlengths LIMIT 2;
+SELECT fullname, ST_ASText(geom) FROM roadlengths LIMIT 2;
+
+DROP TABLE mesaroads;
+
+CREATE TABLE mesaroads as SELECT * from roadlengths;
+
+DROP TABLE roadlengths;
