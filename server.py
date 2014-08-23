2221bb72a6ee775f9a90089c123448c8a61a0651
Peter W <techieshark@gmail.com>
2014-08-23 03:02:25 -0700

    improves argument handling

--- server.py
+++ server.py
@@ -2,6 +2,12 @@ from flask import Flask, render_template, request, url_for, jsonify
 from geocoder import *
 import string
 import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-d", "--development", help="run with development environment", action="store_true")
+args = parser.parse_args()
+
 
 # Usage:
 # To run the server on Heroku: python server.py
@@ -9,13 +15,12 @@ import sys
 # The difference is that on Heroku we pull the database settings out of the DATABASE_URL env variable,
 # and on the local machine we expect database to be "tiger" on "localhost" with no authentication required.
 
-
-if len(sys.argv) == 1:
-	production = True
-	environment = "Production"
-else: # ANY argument passed, assume dev environment.
+if args.development:
 	production = False
 	environment = "Development"
+else:
+	production = True
+	environment = "Production"
 
 
 app = Flask(__name__)
@@ -25,7 +30,7 @@ app.debug = True
 def hello():
 	global production
 	if request.method=='GET':
-		return "Hello! See the Makefile for details on testing this. " + environment + ": # args: " + str(len(sys.argv))
+		return "Hello! See the Makefile for details on testing this. Environment: " + environment
 
 	if request.method=='POST':
 		sentence = request.form['fileupload']
