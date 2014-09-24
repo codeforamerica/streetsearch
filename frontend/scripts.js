73af59e31ad165c6f244719b3a84a21f1d59b057
Andrew Douglass <andrew@wasteddaylight.com>
2014-09-23 23:07:44 -0700

    Got mapping working.

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,15 +1,13 @@
-var map = L.map('map').setView([33.4150, -111.8314], 13);
-
+var map = L.map('map').setView([33.4150, -111.8314], 11);
 L.tileLayer('http://{s}.tiles.mapbox.com/v3/ardouglass.h3mingmm/{z}/{x}/{y}.png', {
     maxZoom: 18
 }).addTo(map);
 
 function geocodeText(text) {
-  console.log(text);
    $.ajax({
     type: 'POST',
     crossDomain: true,
-    url: 'http://findlines.herokuapp.com/',
+    url: 'http://findlines.herokuapp.com',
     data: { fileupload: text},
     dataType: 'json',
     success: populateMap
@@ -19,12 +17,14 @@ function geocodeText(text) {
 function populateMap(line) {
   var districtLines = L.geoJson().addTo(map);
   lines = line.text;
-  $.each(lines, function(line) {
+  _.forEach(lines, function(line) {
     console.log("mapping: " + line[0]);
     districtLines.addData($.parseJSON(line[1]));
   });
 }
 
 $(function() {
-  $("#geocode").on("click", geocodeText($("#geotext").value));
+  $("#geocode").on("click", function() {
+    geocodeText($("#geotext").val());
+  });
 });
