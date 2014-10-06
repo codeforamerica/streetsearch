e396e9de35e326e0fa59e16732736d3e9a8085df
tom buckley <buckley.tom@gmail.com>
2014-10-06 16:52:11 -0700

    adding postgis changes that were lost in merge

--- data/postgis_union_length.sql
+++ data/postgis_union_length.sql
@@ -5,21 +5,11 @@ CREATE TABLE namedroads AS SELECT roadnames.predirabrv as prefix, roadnames.suft
 
 --
 DROP TABLE IF EXISTS mesaroads;
-CREATE TABLE mesaroads AS SELECT fullname, namedroads.geom FROM mesa_boundaries, namedroads WHERE ST_DWithin(mesa_boundaries.geom, namedroads.geom, 0);
+CREATE TABLE roadswithin AS SELECT fullname, namedroads.geom FROM mesa_boundaries, namedroads WHERE ST_DWithin(mesa_boundaries.geom, namedroads.geom, 0);
 
 -- group road rows by name
 DROP TABLE IF EXISTS roadlengths;
-CREATE TABLE roadlengths as select st_union(geom) as geom, fullname from mesaroads group by fullname;
+CREATE TABLE roadswithingrouped as select st_union(geom) as geom, fullname from roadswithin group by fullname;
 SELECT UpdateGeometrySRID('roadlengths','geom',4269);
---
--- add column for length
-ALTER TABLE roadlengths ADD COLUMN length NUMERIC(12,2);
---
--- populate length of roads
-UPDATE roadlengths SET length = ST_Length(ST_Transform(geom,32612));
 
-DROP TABLE mesaroads;
-
-CREATE TABLE mesaroads as SELECT * from roadlengths;
-
-DROP TABLE roadlengths;
+DROP TABLE roadswithin;
