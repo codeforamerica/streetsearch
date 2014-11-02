c46c61e1e018ec4dcd9186b371c0116ee02f0dcd
Peter W <techieshark@gmail.com>
2014-11-02 11:48:26 -0800

    misc code cleanup
    - adding semicolons
    - declaring var el
    - making .5 more obvious as 0.5

--- static/scripts.js
+++ static/scripts.js
@@ -2,9 +2,9 @@ var lg = {};
 
 // mesa latlong = [33.4150, -111.8314]
 // nyc = [40.7210,-73.9823]
-var localized_latlong = [40.7210,-73.9823]
+var localized_latlong = [40.7210,-73.9823];
 
-var placename = "nyc"
+var placename = "nyc";
 
 var map = L.map('map').setView(localized_latlong, 11);
 L.tileLayer('http://{s}.tiles.mapbox.com/v3/ardouglass.h3mingmm/{z}/{x}/{y}.png', {
@@ -19,7 +19,7 @@ function geocodeText(text, placename) {
     dataType: 'json',
     success: populateMap
   });
-};
+}
 
 function populateMap(response) {
   // add a new layer to the map
@@ -27,10 +27,10 @@ function populateMap(response) {
     style: { color: '#0a1e0a', weight: 5 },
     onEachFeature: function (feature, layer) {
 
-      el = $("<div>"+feature.properties.founditem+"</div>");
+      var el = $("<div>"+feature.properties.founditem+"</div>");
       $("#found-roads").append(el);
       if (layer instanceof L.Marker){
-        layer.setOpacity(.5);
+        layer.setOpacity(0.5);
       }
 
       // Show which found item is being hovered over
@@ -44,7 +44,7 @@ function populateMap(response) {
         $(this).css('color', '#329632');
       }, function() {
         if (layer instanceof L.Marker){
-          layer.setOpacity(.5);
+          layer.setOpacity(0.5);
         } else { // assume we're dealing with a line
           layer.setStyle( { color: '#0a1e0a', weight: 5 } );
         }
@@ -126,31 +126,31 @@ $(function() {
 
     if (clickedTab == "chicago") {
       map.panTo(new L.LatLng(41.8376, -87.6818)); //dc GeoPosition[{38.9041, -77.0171}] nyc GeoPosition[{40.7283, -73.9942}] sf GeoPosition[{37.7599, -122.437}]
-      placename = "chicago"
+      placename = "chicago";
     }
     else if (clickedTab == "dc") {
       map.panTo(new L.LatLng(38.9041, -77.0171)); //dc GeoPosition[{38.9041, -77.0171}] nyc GeoPosition[{40.7283, -73.9942}] sf GeoPosition[{37.7599, -122.437}]
-      placename = "dc"
+      placename = "dc";
     }
     else if (clickedTab == "nyc") {
       map.panTo(new L.LatLng(40.7283, -73.9942)); //dc GeoPosition[{38.9041, -77.0171}] nyc GeoPosition[{40.7283, -73.9942}] sf GeoPosition[{37.7599, -122.437}]
-      placename = "nyc"
+      placename = "nyc";
     }
     else if (clickedTab == "sf") {
       map.panTo(new L.LatLng(37.7599, -122.437)); //dc GeoPosition[{38.9041, -77.0171}] nyc GeoPosition[{40.7283, -73.9942}] sf GeoPosition[{37.7599, -122.437}]
-      placename = "sf"
+      placename = "sf";
     }
     else if (clickedTab == "portland") {
       map.panTo(new L.LatLng(45.537, -122.65)); //portland GeoPosition[{45.537, -122.65}] dc GeoPosition[{38.9041, -77.0171}] nyc GeoPosition[{40.7283, -73.9942}] sf GeoPosition[{37.7599, -122.437}]
-      placename = "portland"
+      placename = "portland";
     }
     else if (clickedTab == "dallas") {
       map.panTo(new L.LatLng(32.7942, -96.7655)); //dallas: GeoPosition[{32.7942, -96.7655}] portland GeoPosition[{45.537, -122.65}] dc GeoPosition[{38.9041, -77.0171}] nyc GeoPosition[{40.7283, -73.9942}] sf GeoPosition[{37.7599, -122.437}]
-      placename = "dallas"
+      placename = "dallas";
     }
     else if (clickedTab == "mesa") {
       map.panTo(new L.LatLng(33.4019, -111.717)); //mesa GeoPosition[{}], dallas: GeoPosition[{32.7942, -96.7655}] portland GeoPosition[{45.537, -122.65}] dc GeoPosition[{38.9041, -77.0171}] nyc GeoPosition[{40.7283, -73.9942}] sf GeoPosition[{37.7599, -122.437}]
-      placename = "mesa"
+      placename = "mesa";
     }
     // Only show active tab
     $('.cities .pure-menu-selected').removeClass('pure-menu-selected');
